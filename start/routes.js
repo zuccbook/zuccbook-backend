143db155845421f35fd8315091e8cbfc8b99768d
nkyoko <notorious.droid@gmail.com>
2020-05-28 11:26:55 +0200

    added function to change post

--- start/routes.js
+++ start/routes.js
@@ -97,6 +97,8 @@ Route.delete("/friend/delete", "FriendController.removeFriend").middleware(["spo
 
 Route.post("/post/create", "PostController.createPost").middleware(["spoofAccept", "auth"]);
 
+Route.patch("/post/update", "PostController.UpdatePost").middleware(["spoofAccept", "auth"]);
+
 Route.get("/posts/get", "PostController.getPosts").middleware(["spoofAccept", "auth"]);
 
 Route.get("/post/get", "PostController.getPost").middleware(["spoofAccept", "auth"]);
